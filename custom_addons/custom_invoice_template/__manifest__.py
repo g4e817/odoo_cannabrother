bec5bab8cd0ef458eab6d88106dd2cdf8cede073
Harald Deutschmann <harald@mountain.co.at>
2022-09-28 17:40:32 +0200

    bug fix

--- custom_addons/custom_invoice_template/__manifest__.py
+++ custom_addons/custom_invoice_template/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'postlabel', 'account_payment_sale'],
+    'depends': ['account_payment_partner', 'account', 'postlabel', 'account_payment_sale'],
 
     # always loaded
     'data': [
