9325a1e96d0dc95e8ecf1254ca4de44f0cda021b
Harald Deutschmann <harald@mountain.co.at>
2022-09-28 16:22:55 +0200

    dependency

--- custom_addons/custom_invoice_template/__manifest__.py
+++ custom_addons/custom_invoice_template/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'postlabel'],
+    'depends': ['account', 'postlabel', 'account_payment_sale'],
 
     # always loaded
     'data': [
